a0eceff70be0a38c24454c9a66640ca01d28bb58
Mr. Will <mr.will.com@outlook.com>
2023-04-08 14:44:22 +0800

    Add API fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+'use client'
+
+import { useState, useEffect } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
-  return <></>
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(
+        'https://noc.social/api/v1/accounts/72358/statuses?only_media=true&tagged=photography',
+      )
+      setPosts(await response.json())
+    })()
+  }, [])
+
+  return (
+    <>
+      {posts.flatMap(post =>
+        post.media_attachments.map(attachment => (
+          <img
+            src={attachment.url}
+            style={{ width: 200, height: 200, objectFit: 'cover' }}
+          />
+        )),
+      )}
+    </>
+  )
 }
