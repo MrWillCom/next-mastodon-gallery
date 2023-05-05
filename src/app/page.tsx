f17abfdc0de752c96865629ed8f73249c67ae929
Mr. Will <mr.will.com@outlook.com>
2023-05-05 23:37:26 +0800

    Update fonts and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import { useState, useEffect } from 'react'
 import styles from './page.module.scss'
-import moment from 'moment'
-import { Blurhash } from "react-blurhash";
+import { Blurhash } from 'react-blurhash'
 
-import { Inter } from 'next/font/google'
+import { Noto_Serif } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const notoSerif = Noto_Serif({
+  subsets: ['latin'],
+  weight: '400',
+  style: 'italic',
+})
 
 export default function Home() {
   const [posts, setPosts]: [
@@ -35,33 +38,31 @@ export default function Home() {
   }, [])
 
   return (
-    <main className={styles.main + ' ' + inter.className}>
-      {posts.map(post => (
-        <div className={styles.post} key={post.id}>
-          <div className={styles.header}>
-            <h1 className={styles.date}>
-              {moment(post.created_at).format('ll')}
-            </h1>
-            <div
-              className={styles.description}
-              dangerouslySetInnerHTML={{ __html: post.content }}
-            ></div>
-          </div>
-          <div className={styles.attachments}>
-            {post.media_attachments.map(attachment => (
-              <div className={styles.attachment} key={attachment.id}>
-                <Blurhash hash={attachment.blurhash} className={styles.blurhash} width="100%" height="auto" />
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={attachment.url}
-                  className={styles.image}
-                  alt={attachment['description']}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </main>
+    <div className={notoSerif.className}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Gallery</h1>
+        <p className={styles.subtitle}>by Mr. Will</p>
+      </header>
+      <main className={styles.main}>
+        {posts.flatMap(post =>
+          post.media_attachments.map(attachment => (
+            <div className={styles.attachment} key={attachment.id}>
+              <Blurhash
+                hash={attachment.blurhash}
+                className={styles.blurhash}
+                width="100%"
+                height="auto"
+              />
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={attachment.url}
+                className={styles.image}
+                alt={attachment['description']}
+              />
+            </div>
+          )),
+        )}
+      </main>
+    </div>
   )
 }
